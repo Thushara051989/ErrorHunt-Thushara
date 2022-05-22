e9b3103afb9d55bd1ed9c51cfacc552e29abe954
Thushara051989 <102418353+Thushara051989@users.noreply.github.com>
2022-05-22 15:37:42 +0530

    make it better

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.set('views','./src/views');
 app.set('view engine','ejs'); 
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname , '/public'))); 
 
